fb5c95def1becbc75267b44dacfe00f44deac219
MARK <mark@MARKs-MacBook-Pro.local>
2024-03-06 00:17:42 +0300

    created sign in, sign up, forgot password UIs and OAauth button

---
+++ src/components/OAuth.jsx
@@ -0,0 +1,12 @@
+// import { FcGoogle } from "react-icons/fc"
+
+const OAuth = () => {
+  return (
+    <button className="flex items-center justify-center w-full bg-red-600 text-white px-7 py-3 rounded uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transitio duration-150 ease-in-out">
+        {/* <FcGoogle className="bg-white rounded-full text-2xl mr-2"/> */}
+      Continue with Google
+    </button>
+  )
+};
+
+export default OAuth
--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,10 +1,70 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import OAuth from "../components/OAuth";
 
 const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+  }
+    
   return (
-    <div>
-      ForgotPassword
-    </div>
-  )
+    <section>
+      <h1 className="text-3xl text-center mt-6 font-semibold">Forgot Password</h1>
+      <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
+        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+          <img
+            src="https://plus.unsplash.com/premium_photo-1663089688180-444ff0066e5d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGtleXxlbnwwfHwwfHx8MA%3D%3D"
+            alt="Sign-In"
+            className="w-full rounded-md"
+          />
+        </div>
+        <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
+          <form>
+            <input
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+              type="email"
+              id="email"
+              value={email}
+              onChange={handleChange}
+              placeholder="Email address"
+            />
+            
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
+              <p className="mb-6">
+                Don't have an account?
+                <Link
+                  to="/sign-up"
+                  className="ml-1 text-red-500 hover:text-red-700 transition duration-200 ease-in-out"
+                >
+                  Sign up.
+                </Link>
+              </p>
+              <p>
+                <Link
+                  to="/sign-in"
+                  className=" text-blue-500 hover:text-blue-700 transition duration-200 ease-in-out"
+                >
+                  Sign in instead
+                </Link>
+              </p>
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-600 px-7 py-3 text-sm text-white uppercase rounded shadow-md hover:text-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
+            >
+              Send Reset Password
+            </button>
+            <div className="flex items-center my-4 before:border-t before:flex-1  before:border-gray-300 after:border-t after:flex-1  after:border-gray-300">
+              <p className="text-center font-semibold mx-4">OR</p>
+            </div>
+            <OAuth/>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 };
 
-export default ForgotPassword
+export default ForgotPassword;
