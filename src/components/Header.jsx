ca4c73aad1ffe2d68f40950d9dc62ff6e0032f7c
MARK <mark@MARKs-MacBook-Pro.local>
2024-03-05 11:21:40 +0300

    setup cra, install tailwindcss, create  pages

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+
+const Header = () => {
+  return (
+    <>
+    <div>
+        <header>
+            <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="logo" className="h-5" />
+        </header>
+
+
+    </div>
+    
+    </>
+  )
+};
+
+export default Header
