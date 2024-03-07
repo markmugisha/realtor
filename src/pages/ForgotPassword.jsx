a9acbf5d8d14177801c4051cdfdca0cb7875624f
Mark Mugisha <141035540+markmugisha@users.noreply.github.com>
2024-03-07 22:44:50 +0300

    complete forgot password functionality

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,70 +1,82 @@
-import React, { useState } from "react";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 import OAuth from "../components/OAuth";
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState("");
 
-  const handleChange = (e) => {
+  const onChange = (e) => {
     setEmail(e.target.value);
   }
-    
+
+  const onSubmit = async(e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Email was sent");
+    } catch (error) {
+      toast.error("Could not send reset password");
+    }
+  }
   return (
     <section>
-      <h1 className="text-3xl text-center mt-6 font-semibold">Forgot Password</h1>
+      <h1 className="text-3xl text-center mt-6 font-bold">Forgot Password</h1>
       <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
         <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
           <img
-            src="https://plus.unsplash.com/premium_photo-1663089688180-444ff0066e5d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGtleXxlbnwwfHwwfHx8MA%3D%3D"
-            alt="Sign-In"
-            className="w-full rounded-md"
+            src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
+            alt="key"
+            className="w-full rounded-2xl"
           />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onSubmit}>
             <input
-              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
               type="email"
               id="email"
               value={email}
-              onChange={handleChange}
+              onChange={onChange}
               placeholder="Email address"
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
             />
-            
+
             <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
               <p className="mb-6">
-                Don't have an account?
+                Don't have a account?
                 <Link
                   to="/sign-up"
-                  className="ml-1 text-red-500 hover:text-red-700 transition duration-200 ease-in-out"
+                  className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1"
                 >
-                  Sign up.
+                  Register
                 </Link>
               </p>
               <p>
                 <Link
                   to="/sign-in"
-                  className=" text-blue-500 hover:text-blue-700 transition duration-200 ease-in-out"
+                  className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out"
                 >
                   Sign in instead
                 </Link>
               </p>
             </div>
             <button
+              className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
               type="submit"
-              className="w-full bg-blue-600 px-7 py-3 text-sm text-white uppercase rounded shadow-md hover:text-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
             >
-              Send Reset Password
+              Send reset password
             </button>
-            <div className="flex items-center my-4 before:border-t before:flex-1  before:border-gray-300 after:border-t after:flex-1  after:border-gray-300">
+            <div className="flex items-center  my-4 before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
               <p className="text-center font-semibold mx-4">OR</p>
             </div>
-            <OAuth/>
+            <OAuth />
           </form>
         </div>
       </div>
     </section>
   );
-};
+}
 
 export default ForgotPassword;
