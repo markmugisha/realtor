a2df6b9336549e006c7a427f5f4a8e0dfeab0c01
Mark Mugisha <141035540+markmugisha@users.noreply.github.com>
2024-03-08 09:31:08 +0300

    create private route and custom hook for protecting the profile page and add logout functionality

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import ForgotPassword from './pages/ForgotPassword';
-import Home from './pages/Home';
-import Offers from './pages/Offers';
-import Profile from './pages/Profile';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Header from './components/Header';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <>
-     <Router>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/forgot-password" element={<ForgotPassword/>} />
-        <Route path="/offers" element={<Offers/>} />
-        <Route path="/profile" element={<Profile/>} />
-        <Route path="/sign-in" element={<SignIn/>} />
-        <Route path="/sign-up" element={<SignUp/>} />
-      </Routes> 
-    </Router>
-    <ToastContainer
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </Router>
+      <ToastContainer
         position="bottom-center"
         autoClose={5000}
         hideProgressBar={false}
@@ -35,7 +38,7 @@ function App() {
         pauseOnHover
         theme="dark"
       />
-    </>  
+    </>
   );
 }
 
