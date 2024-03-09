3780dcb2fbb68197740c85cde41c7d0e3e8a4b60
Mark Mugisha <141035540+markmugisha@users.noreply.github.com>
2024-03-09 22:10:01 +0300

    complete header and making it dynamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,27 @@
+import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-
-
-const Header = () => {
-const location = useLocation();
-const navigate = useNavigate();
-
-const pathMathRoute = (route) => {
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+export default function Header() {
+  const [pageState, setPageState] = useState("Sign in");
+  const location = useLocation();
+  const navigate = useNavigate();
+  const auth = getAuth();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setPageState("Profile");
+      } else {
+        setPageState("Sign in");
+      }
+    });
+  }, [auth]);
+  function pathMatchRoute(route) {
     if (route === location.pathname) {
       return true;
     }
-}
-
+  }
   return (
-    <div className="bg-white border-b shadow-sm sticky top-0 z-0">
+    <div className="bg-white border-b shadow-sm sticky top-0 z-40">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
           <img
@@ -23,15 +32,35 @@ const pathMathRoute = (route) => {
           />
         </div>
         <div>
-          <ul className="flex space-x-10 cursor-pointer">
-            <li className={`py-3 text-gray-400 font-semibold text-sm border-b-[3px] border-b-transparent ${pathMathRoute("/") && "text-black border-b-red-500" }`} onClick={() => navigate("/")}>Home</li>
-            <li className={`py-3 text-gray-400 font-semibold text-sm border-b-[3px] border-b-transparent ${pathMathRoute("/offers") && "text-black border-b-red-500" }`} onClick={() => navigate("/offers")}>Offers</li>
-            <li className={`py-3 text-gray-400 font-semibold text-sm border-b-[3px] border-b-transparent ${pathMathRoute("/sign-in") && "text-black border-b-red-500" }`} onClick={() => navigate("/sign-in")}>Sign in</li>
+          <ul className="flex space-x-10">
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/")}
+            >
+              Home
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/offers") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/offers")}
+            >
+              Offers
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                (pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) &&
+                "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/profile")}
+            >
+              {pageState}
+            </li>
           </ul>
         </div>
       </header>
     </div>
   );
-};
-
-export default Header;
+}
