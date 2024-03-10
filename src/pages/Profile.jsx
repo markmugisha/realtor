4a1afec512301989e1a209334420b36b39ce33dd
Mark Mugisha <141035540+markmugisha@users.noreply.github.com>
2024-03-10 11:36:33 +0300

    create the create listing UI

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,10 @@
 import { getAuth, updateProfile } from "firebase/auth";
 import { doc, updateDoc } from "firebase/firestore";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { db } from "../firebase";
-
+import { FcHome } from "react-icons/fc";
 
 export default function Profile() {
   const auth = getAuth();
@@ -46,39 +46,39 @@ export default function Profile() {
       toast.error("Could not update the profile details");
     }
   }
-return (
-<>
-<section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
-  <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
-  <div className="w-full md:w-[50%] mt-6 px-3">
-    <form>
-      {/* Name Input */}
+  return (
+    <>
+      <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
+        <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
+        <div className="w-full md:w-[50%] mt-6 px-3">
+          <form>
+            {/* Name Input */}
 
-      <input
-        type="text"
-        id="name"
-        value={name}
-        disabled={!changeDetail}
-        onChange={onChange}
-        className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${
-          changeDetail && "bg-red-200 focus:bg-red-200"
-        }`}
-      />
+            <input
+              type="text"
+              id="name"
+              value={name}
+              disabled={!changeDetail}
+              onChange={onChange}
+              className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${
+                changeDetail && "bg-red-200 focus:bg-red-200"
+              }`}
+            />
 
-      {/* Email Input */}
+            {/* Email Input */}
 
-      <input
-        type="email"
-        id="email"
-        value={email}
-        disabled
-        className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
-      />
+            <input
+              type="email"
+              id="email"
+              value={email}
+              disabled
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
+            />
 
-      <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
-        <p className="flex items-center ">
-          Do you want to change your name?
-          <span
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
+              <p className="flex items-center ">
+                Do you want to change your name?
+                <span
                   onClick={() => {
                     changeDetail && onSubmit();
                     setChangeDetail((prevState) => !prevState);
@@ -86,18 +86,24 @@ return (
                   className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer"
                 >
                   {changeDetail ? "Apply change" : "Edit"}
-          </span>
-        </p>
-        <p
-          onClick={onLogout}
-          className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer"
-        >
-          Sign out
-        </p>
-      </div>
-    </form>
-  </div>
-</section>
-</>
-);
+                </span>
+              </p>
+              <p
+                onClick={onLogout}
+                className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer"
+              >
+                Sign out
+              </p>
+            </div>
+          </form>
+          <button type="submit" className="w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800">
+            <Link to='/create-listing' className="flex justify-center items-center">
+              <FcHome className="mr-2 text-2xl bg-red-500 rounded-full p-1 border-2"/>
+              Sell or rent your property
+            </Link>
+          </button>
+        </div>
+      </section>
+    </>
+  );
 }
